edd7b58dcb7466d493134fbc7409b6aaa54f965e
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-11-01 21:31:20 +0000

    Board added with pictures

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let cardElements = document.getElementsByClassName('game-card');
+let cardElementsArray = [...cardElements];
+let imgElements = document.getElementsByClassName('game-card-img');
+let imgElementsArray = [...imgElements];
+
