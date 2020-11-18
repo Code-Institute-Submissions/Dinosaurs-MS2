87555dccae0eb5e3f1efbb29dfe9c2ca80b20ce3
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-11-18 10:16:20 +0000

    Start Card again

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-let cardElements = document.getElementsByClassName('game-card');
-let cardElementsArray = [...cardElements];
-let imgElements = document.getElementsByClassName('game-card-img');
-let imgElementsArray = [...imgElements];
-
