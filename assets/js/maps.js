ae6887f7ac36de843bb922fde218d134877ae218
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-03 21:33:36 +0000

    Google Maps Pages Added

---
+++ assets/js/maps.js
@@ -0,0 +1,58 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7.2,
+        center: {
+            lat: 52.746590,
+            lng: -1.473730
+        }
+    });
+
+  const infoWin = new google.maps.InfoWindow();
+  const markers = locations.map(function(location, i) {
+  const marker = new google.maps.Marker({
+      position: location
+    });
+    google.maps.event.addListener(marker, 'click', function(evt) {
+      infoWin.setContent(location.info);
+      infoWin.open(map, marker);
+    })
+    return marker;
+  });
+  const markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
+
+    const locations = [
+        { lat: 52.37872, 
+          lng: -2.29157,
+          info: 'West Midlands Safari park',
+          link: 'http://www.wmsp.co.uk'},
+        { lat: 51.49656, 
+          lng: -0.17689,
+          info: 'National History Museum'},
+        { lat: 50.71475, 
+          lng: -2.43398, 
+          info:'The Dinosaur Museum'},
+        { lat: 51.18897, 
+          lng: -4.00354,
+          info:'Combe Martin Wildlife & Dinosaur Park' },
+        { lat: 50.72533, 
+          lng: -2.93308,
+          info: 'Dinosaurland Fossil Museum' },
+        { lat: 53.40999, 
+          lng: -2.98132,
+          info: 'World Museum' },
+        { lat: 51.74457, 
+          lng: -0.06130,
+          info:'World of Dinosaurs - Paradise Wildlife Park' },
+        { lat: 51.42046, 
+          lng: -0.07102,
+           info:'Crystal Palace Park' },
+        { lat: 51.28049, 
+          lng: -1.23544,
+          info:'Wingham Wildlife Park' },
+        { lat: 50.65986, 
+          lng: -1.14155,
+          info:'Dinosaur Isle' }
+    ];
