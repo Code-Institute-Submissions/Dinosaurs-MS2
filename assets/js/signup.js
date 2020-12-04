9017831fe967dccad1d5007d800c5a2b426ea91f
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-04 12:44:31 +0000

    Signup Page configured to EMail.JS

---
+++ assets/js/signup.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "dino", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_contactnumber": contactForm.contactnumber.value
+    })
+    .then(
+        function(response) {
+            alert('Many thanks for signing up to Dinosaur Discoveries!')
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
