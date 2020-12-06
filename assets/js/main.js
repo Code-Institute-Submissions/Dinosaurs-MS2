7db06f759d9fbda8240731afc525c265f9275e00
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-06 21:07:34 +0000

    Beautify Coding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard, secondCard;
+let firstCard,
+    secondCard;
 
 let moves = 0;
 let count = 0;
@@ -18,118 +19,129 @@ const flip = new Audio("assets/sounds/flip.mp3");
 const finish = new Audio("assets/sounds/finish.mp3");
 
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-  flip.play();
-  this.classList.add('flip');
-  
+    if (lockBoard) 
+        return;
+    if (this === firstCard) 
+        return;
+    flip.play();
+    this
+        .classList
+        .add('flip');
 
-   if (!hasFlippedCard) {
-     hasFlippedCard = true;
-     firstCard = this;
-     return;
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
     }
- 
+
     secondCard = this;
-    
+
     checkForMatch();
     moveCounter();
-    
-    
-  }
- 
-  function checkForMatch() {
-      
+
+}
+
+function checkForMatch() {
+
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
-    
-    
-  }
- 
-  function disableCards() {
+    isMatch
+        ? disableCards()
+        : unflipCards();
+
+}
+
+function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     countCounter();
     roar.play();
     resetBoard();
     congrats();
-  }
- 
-  function unflipCards() {
+}
+
+function unflipCards() {
     lockBoard = true;
     setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
-      resetBoard();
+        firstCard
+            .classList
+            .remove('flip');
+        secondCard
+            .classList
+            .remove('flip');
+        resetBoard();
     }, 1000);
-  }
+}
 
-  function resetBoard() {
+function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-  }
-
-  (function shuffle() {
+}(function shuffle() {
     cards.forEach(card => {
-      let ramdomPos = Math.floor(Math.random() * 16);
-      card.style.order = ramdomPos;
+        let ramdomPos = Math.floor(Math.random() * 16);
+        card.style.order = ramdomPos;
     });
-  })();
+})();
 
-
-  function moveCounter(){
+function moveCounter() {
     moves++;
     counter.innerHTML = moves;
-    if (moves > 14 && moves < 22){
-      for( i= 0; i < 3; i++){
-          if(i > 1){
-              stars[i].style.visibility = "collapse";
-          }
-      }
-  }
-  else if (moves > 23){
-      for( i= 0; i < 3; i++){
-          if(i > 0){
-              stars[i].style.visibility = "collapse";
-          }
-      }
-  }
-    
-  }
-  
-  function countCounter(){
+    if (moves > 14 && moves < 22) {
+        for (i = 0; i < 3; i++) {
+            if (i > 1) {
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    } else if (moves > 23) {
+        for (i = 0; i < 3; i++) {
+            if (i > 0) {
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+
+}
+
+function countCounter() {
     count++;
     counter2.innerHTML = count;
-  }
+}
 
-  function congrats(){
-    if (count == 8){
-        modal.classList.add("show");
+function congrats() {
+    if (count == 8) {
+        modal
+            .classList
+            .add("show");
         roar.pause();
         finish.play();
-        
-        var starRating = document.querySelector(".stars").innerHTML;
 
-        document.getElementById("finalMove").innerHTML = moves+1;
-        document.getElementById("starRating").innerHTML = starRating;
+        var starRating = document
+            .querySelector(".stars")
+            .innerHTML;
+
+        document
+            .getElementById("finalMove")
+            .innerHTML = moves + 1;
+        document
+            .getElementById("starRating")
+            .innerHTML = starRating;
 
         closeModal();
     };
-  }
-
-  function closeModal(){
-  closeicon.addEventListener("click", function(e){
-      modal.classList.remove("show");
-      restart();
-  });
 }
 
-  function restart (){
+function closeModal() {
+    closeicon.addEventListener("click", function (e) {
+        modal
+            .classList
+            .remove("show");
+        restart();
+    });
+}
+
+function restart() {
 
     location.reload();
     return false;
-  }
-
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
