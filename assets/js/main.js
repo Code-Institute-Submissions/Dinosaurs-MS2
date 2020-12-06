dd9acbd5e081a468726d5422747732c222fad3f2
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-06 13:20:47 +0000

    Star Rating and Scoring Modal Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,14 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 let moves = 0;
+let count = 0;
 let counter = document.querySelector(".moves-counter");
+let counter2 = document.querySelector(".count-counter");
+
+const stars = document.querySelectorAll(".fa-star");
 let starsList = document.querySelectorAll(".stars li");
+let closeicon = document.querySelector(".close");
+let modal = document.getElementById("popup1")
 
 const roar = new Audio("assets/sounds/roar.mp3");
 const flip = new Audio("assets/sounds/flip.mp3");
@@ -36,13 +42,16 @@ function flipCard() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
     
+    
   }
  
   function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    countCounter();
     roar.play();
     resetBoard();
+    congratulations();
   }
  
   function unflipCards() {
@@ -66,11 +75,51 @@ function flipCard() {
     });
   })();
 
+
   function moveCounter(){
     moves++;
     counter.innerHTML = moves;
+    if (moves > 14 && moves < 22){
+      for( i= 0; i < 3; i++){
+          if(i > 1){
+              stars[i].style.visibility = "collapse";
+          }
+      }
+  }
+  else if (moves > 23){
+      for( i= 0; i < 3; i++){
+          if(i > 0){
+              stars[i].style.visibility = "collapse";
+          }
+      }
+  }
     
   }
+  
+  function countCounter(){
+    count++;
+    counter2.innerHTML = count;
+  }
+
+  function congratulations(){
+    if (count == 8){
+        modal.classList.add("show");
+
+        var starRating = document.querySelector(".stars").innerHTML;
+
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+
+        closeModal();
+    };
+  }
+
+  function closeModal(){
+  closeicon.addEventListener("click", function(e){
+      modal.classList.remove("show");
+      restart();
+  });
+}
 
   function restart (){
 
@@ -78,4 +127,6 @@ function flipCard() {
     return false;
   }
 
+
 cards.forEach(card => card.addEventListener('click', flipCard));
+card.addEventListener("click",congratulations);
