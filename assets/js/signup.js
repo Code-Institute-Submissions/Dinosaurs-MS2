bd1749f87781be5ad38573095432100c00a9b57e
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-07 22:28:15 +0000

    Signup Page completed, sound added, footer added

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,18 +1,26 @@
+const signup = new Audio("assets/sounds/signup.mp3");
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "dino", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "from_contactnumber": contactForm.contactnumber.value
     })
+  
     .then(
-        function(response) {
-            alert('Many thanks for signing up to Dinosaur Discoveries!')
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
+        // Change button text to green and display 'sent' message
+        function () {
+            let sentButton = document.getElementById('contact-button');
+            signup.play();
+            sentButton.style.backgroundColor = "Green";
+            sentButton.innerHTML = "Sign Up Complete!";
 
+        },
+        // On error, using standard browser alert
+        function (error) {
+            alert("Whoops! An error occurred, please try again.", error);
+        });
+        // Clear form after submission
+        document.getElementById('contactForm').reset();
+return false;
+}
