404c5303356636c231f5f3dac83fd97e770123b9
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2020-12-08 21:19:35 +0000

    commet added to Dinoinfo.js

--- assets/js/dinoinfo.js
+++ assets/js/dinoinfo.js
@@ -1,3 +1,5 @@
+
+// Function to show the dinosaur information and picture when the dino selector is changed
 $(document).on('change', '.dino-selector', function() {
     var target = $(this).data('target');
     var show = $("option:selected", this).data('show');
